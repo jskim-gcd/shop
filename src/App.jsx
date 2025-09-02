126e11342c57d2d82eac3a6a5bc92e6c0f673f08
Kim Jinsoo <jskimcs1@gmail.com>
2025-09-02 16:06:49 +0900

    Navbar 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    
+    <>
+      <Navbar bg="dark" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home" onClick={() => {}}>Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link>home</Nav.Link>
+            <Nav.Link>cart</Nav.Link>
+            <Nav.Link>wishlist</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
   )
 }
 
