32739b2be80a57c4d432c6cab63bfd3d594a0fb4
Kim Jinsoo <jskimcs1@gmail.com>
2025-09-02 22:44:09 +0900

    메인 페이지 제품 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,40 @@
 import { useSelector } from "react-redux";
 import bg from '../img/bg.png'
+import { Col, Container, Row } from "react-bootstrap";
 
 function Main() {
+
     let shoes = useSelector((state) => state.shoes)
 
     return (
         <>
-            main
+            <div className="main-bg" style={{ backgroundImage: 'url(' + bg + ')' }} />
+
+            <div>
+                <Container>
+                    <Row>
+                        {
+                            shoes.map((a, i) => {return (
+                                <Card key={i} i={i} shoes={a} />
+                            )})
+                        }
+                    </Row>
+                </Container>
+            </div>
         </>
     )
+}
+
+function Card(props) {
+
+    return(
+        <Col sm={4}>
+            <img src={'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg'} width='80%' />
+            <h4>{props.shoes.title}</h4>
+            <p>{props.shoes.content}</p>
+            <p>{props.shoes.price}</p>
+        </Col>
+    ) 
 }
 
 export default Main
